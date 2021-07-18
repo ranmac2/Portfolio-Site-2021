961b1410a3ae706ca47c5ed9e62036753e79778d
Randy <randy_maclean@hotmail.ca>
2021-07-17 23:32:38 -0700

    Trying Javascript slide in animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const slideRightElement = document.querySelector('.about-me-content');
+
+const slideRightElementOptions = {};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+          return;
+      } else {
+        entry.target.classList.add('slide-right-element');
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  }, slideRightElementOptions);
+
+appearOnScroll.observe(slideRightElement);
+  
+
+  
+
 let firstName = document.getElementById('first-name');
 let lastName = document.getElementById('last-name');
 let email = document.getElementById('email');
