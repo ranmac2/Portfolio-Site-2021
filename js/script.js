ec62d578ce64f1cc2770973ba681537650bfe996
Randy <randy_maclean@hotmail.ca>
2021-07-20 21:29:51 -0700

    Added animations to sections

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-const slideRightElement = document.querySelector('.about-me-content');
+const fadeRight = document.querySelector(".fade-right");
+const fadeLeft = document.querySelector(".fade-left");
+const fadeUp = document.querySelector(".fade-up");
 
-const slideRightElementOptions = {};
+
+const appearOptions = {
+};
 
 const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
     entries.forEach(entry => {
-      if (!entry.isIntersecting) {
-          return;
-      } else {
-        entry.target.classList.add('slide-right-element');
-        appearOnScroll.unobserve(entry.target);
-      }
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
     });
-  }, slideRightElementOptions);
+  }, appearOptions);
 
-appearOnScroll.observe(slideRightElement);
-  
+window.onscroll = function() {scrollFunction()};
 
-  
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("homepage-header").style.fontSize = "20px";
+        document.getElementById("homepage-header").style.flexDirection = "row";
+        document.getElementById("homepage-header").style.height = "65px";
+        document.getElementById("logo").style.height = "75px";
+        document.getElementById("logo").style.width = "75px";
+        document.getElementById("homepage-header").style.justifyContent = "space-between";
+        document.getElementById("homepage-header").style.zIndex= "1";
+    } else {
+        document.getElementById("homepage-header").style.fontSize = "25px";
+        document.getElementById("homepage-header").style.flexDirection = "column";
+        document.getElementById("homepage-header").style.height = "200px";
+        document.getElementById("logo").style.height = "125px";
+        document.getElementById("logo").style.width = "125px";
+    }
+}
 
-let firstName = document.getElementById('first-name');
-let lastName = document.getElementById('last-name');
-let email = document.getElementById('email');
+appearOnScroll.observe(fadeRight);
+appearOnScroll.observe(fadeLeft);
+appearOnScroll.observe(fadeUp);
