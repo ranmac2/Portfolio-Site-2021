23088b8a76d091957b6ca9fb857cf7e33a331e51
Randy <randy_maclean@hotmail.ca>
2021-07-31 21:42:26 -0700

    fixed overflow issue with menu and styled h1 and linked nav a hrefs

--- js/portfolio-page-script.js
+++ js/portfolio-page-script.js
@@ -19,7 +19,29 @@ function openProjects(evt, projectName) {
     evt.currentTarget.className += " active";
   }
 
-  
+  openProjects();
 
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("main-header").style.fontSize = "25px";
+        document.getElementById("main-header").style.height = "115px";
+        document.getElementById("main-header").style.justifyContent = "space-between";
+        document.getElementById("main-header").style.zIndex= "1";
+        document.getElementById("main-header").style.backgroundColor = "rgba(255,255,255, 0.75)";
+        document.getElementsByTagName("a")[0].style.color = "rgb(34,33,33)";
+        document.getElementsByTagName("a")[1].style.color = "rgb(34,33,33)";
+        document.getElementsByTagName("a")[2].style.color = "rgb(34,33,33)";
+        document.getElementById("logo").src = "../img/red-logo-2.png";
+        
+    } else {
+        document.getElementById("main-header").style.fontSize = "25px";
+        document.getElementById("main-header").style.height = "200px";
+        document.getElementById("main-header").style.backgroundColor = "transparent";
+        document.getElementsByTagName("a")[0].style.color = "white";
+        document.getElementsByTagName("a")[1].style.color = "white";
+        document.getElementsByTagName("a")[2].style.color = "white";
+        document.getElementById("logo").src = "../img/red-logo-3.png";
+    }
+}
 
-openProjects();
+window.onscroll = function() {scrollFunction()};
