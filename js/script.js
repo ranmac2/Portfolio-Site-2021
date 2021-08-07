12e0375be262464f73d161ad0208dcea95c494d1
Randy <randy_maclean@hotmail.ca>
2021-08-07 12:31:34 -0700

    removed logo animation page as it is useless, main homepage is first page now

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ function scrollFunction() {
         document.getElementsByTagName("a")[0].style.color = "rgb(34,33,33)";
         document.getElementsByTagName("a")[1].style.color = "rgb(34,33,33)";
         document.getElementsByTagName("a")[2].style.color = "rgb(34,33,33)";
-        document.getElementById("logo").src = "../img/red-logo-2.png";
+        document.getElementById("logo").src = "img/red-logo-2.png";
         
     } else {
         document.getElementById("main-header").style.fontSize = "25px";
@@ -38,7 +38,7 @@ function scrollFunction() {
         document.getElementsByTagName("a")[0].style.color = "white";
         document.getElementsByTagName("a")[1].style.color = "white";
         document.getElementsByTagName("a")[2].style.color = "white";
-        document.getElementById("logo").src = "../img/red-logo-3.png";
+        document.getElementById("logo").src = "img/red-logo-3.png";
     }
 }
 
